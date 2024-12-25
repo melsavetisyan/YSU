ecf909f6f4365d12ac798c10c11057dd761a69ba
melsavetisyan <109077883+melsavetisyan@users.noreply.github.com>
2024-12-26 01:20:56 +0400

    Delete Exams/1_course/1_semester/2_midterm directory

--- Exams/1_course/1_semester/2_midterm/test_2/ex2/main.cpp
+++
@@ -1,8 +0,0 @@
-// 2. Գրել անդրադարձ ֆունկցիա, որը տրված բնական թվի համար վերադարձնում է այդ թվի թվանշանների քանակը:
-#include <iostream>
-using namespace std;
-
-unsigned rec_len(unsigned n)
-{
-    return (n < 10) ? 1 : 1 + rec_len(n / 10);
-}
